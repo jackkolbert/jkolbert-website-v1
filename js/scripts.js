e59990cc39cf59df973802c582aa8f930d0b11e8
jackkolbert <jkolbert@umich.edu>
2024-09-16 00:26:53 -0400

    added many images, favicon, experience section

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,32 @@
-$(document).ready(function() {
-    $("#demo").html("Hello, World!");
+// bootstrap check
+// $(document).ready(function() {
+//     $("#demo").html("Hello, World!");
+// });
+
+// fade in
+document.addEventListener('DOMContentLoaded', () => {
+    const fadeInElements = document.querySelectorAll('.fade-in-element');
+
+    const observer = new IntersectionObserver((entries, observer) => {
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add('fade-in');
+                observer.unobserve(entry.target);
+            }
+        });
+    }, {
+        threshold: 0.1
+    });
+
+    fadeInElements.forEach(element => {
+        observer.observe(element);
+    });
+});
+
+// tooltips
+document.addEventListener('DOMContentLoaded', function() {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+    var tooltipList = tooltipTriggerList.map(function(tooltipTriggerEl) {
+        return new bootstrap.Tooltip(tooltipTriggerEl);
+    });
 });
